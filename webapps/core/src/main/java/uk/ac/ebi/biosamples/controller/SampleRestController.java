f362863c7116143b1c5b5b1cc5bd91883c1dc0e4
Kerruba <luca.cherubin@gmail.com>
2017-06-13 14:03:39 +0100

    Produce ld+json for accession.ldjson result

--- webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/SampleRestController.java
+++ webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/SampleRestController.java
@@ -1,43 +1,23 @@
 package uk.ac.ebi.biosamples.controller;
 
-import java.net.URI;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.concurrent.TimeUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.EntityLinks;
-import org.springframework.hateoas.ExposesResourceFor;
-import org.springframework.hateoas.MediaTypes;
-import org.springframework.hateoas.PagedResources;
-import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.*;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import uk.ac.ebi.biosamples.model.JsonLDSample;
 import uk.ac.ebi.biosamples.model.Sample;
-import uk.ac.ebi.biosamples.service.FilterService;
-import uk.ac.ebi.biosamples.service.SamplePageService;
-import uk.ac.ebi.biosamples.service.SampleResourceAssembler;
-import uk.ac.ebi.biosamples.service.SampleService;
+import uk.ac.ebi.biosamples.service.*;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Primary controller for REST operations both in JSON and XML and both read and
@@ -60,16 +40,20 @@ public class SampleRestController {
 	private final SampleResourceAssembler sampleResourceAssembler;
 
 	private final EntityLinks entityLinks;
+	
+    private final JsonLDService jsonLDService;
 
-	private Logger log = LoggerFactory.getLogger(getClass());
+    private Logger log = LoggerFactory.getLogger(getClass());
 
 	public SampleRestController(SampleService sampleService, 
 			SamplePageService samplePageService,FilterService filterService,
-			SampleResourceAssembler sampleResourceAssembler, EntityLinks entityLinks) {
+			SampleResourceAssembler sampleResourceAssembler, EntityLinks entityLinks,
+            JsonLDService jsonLDService) {
 		this.sampleService = sampleService;
 		this.samplePageService = samplePageService;
 		this.filterService = filterService;
 		this.sampleResourceAssembler = sampleResourceAssembler;
+		this.jsonLDService = jsonLDService;
 		this.entityLinks = entityLinks;
 	}
 
@@ -141,6 +125,38 @@ public class SampleRestController {
 				//.header(HttpHeaders.CACHE_CONTROL, CacheControl.maxAge(1, TimeUnit.MINUTES).cachePublic().getHeaderValue())
 				.eTag(String.valueOf(sample.hashCode())).contentType(MediaTypes.HAL_JSON).body(sampleResource);
 	}
+
+    @CrossOrigin(methods = RequestMethod.GET)
+    @GetMapping(value = "/{accession}", produces = "application/ld+json")
+    public ResponseEntity<JsonLDSample> getJsonLDSample(@PathVariable String accession) {
+        Sample sample = null;
+        try {
+            sample = sampleService.fetch(accession);
+        } catch (IllegalArgumentException e) {
+            // did not exist, throw 404
+            return ResponseEntity.notFound().build();
+        }
+        if (sample.getName() == null) {
+            // if it has no name, then its just created by accessioning or
+            // reference
+            // can't read it, but could put to it
+            // TODO make sure "options" is correct for this
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+        }
+
+        // check if the release date is in the future and if so return it as
+        // private
+        if (sample.getRelease().isAfter(LocalDateTime.now())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        JsonLDSample jsonLDSample = jsonLDService.sampleToJsonLD(sample);
+
+        // create the response object with the appropriate status
+        return ResponseEntity.ok().lastModified(sample.getUpdate().toEpochSecond(ZoneOffset.UTC))
+                //.header(HttpHeaders.CACHE_CONTROL, CacheControl.maxAge(1, TimeUnit.MINUTES).cachePublic().getHeaderValue())
+                .eTag(String.valueOf(sample.hashCode())).body(jsonLDSample);
+    }
 
 	@PutMapping(value = "/{accession}", consumes = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<Resource<Sample>> put(@PathVariable String accession, @RequestBody Sample sample) {
